76077d8237402dfd61eb2cbe35532a0d0abfe82e
alireza <alirezaii802@gmail.com>
2023-11-28 22:59:26 +0330

    add model product and category and login templates

---
+++ products/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Product, Category
+
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'status', 'datetime_created']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title']
